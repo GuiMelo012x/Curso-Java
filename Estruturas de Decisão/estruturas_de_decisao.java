8d1a8cafe12907f7c4291fc0bcf96a47e999d446
Guilherme Melo <112731659+GuiMelo012x@users.noreply.github.com>
2024-01-19 14:11:31 -0300

    Estruturas de decisão

--- Estruturas de Decisão/estruturas_de_decisao.java
+++ Estruturas de Decisão/estruturas_de_decisao.java
@@ -25,28 +25,28 @@ public class estruturas_de_decisao {
         }
         Com operador ternário, dá pra fazer isso tudo em uma linha: */
         num = (valor > 0) ? valor : 7; // Se o valor for menor que zero, ele vira 7. Se for maior que zero, continua o mesmo.
-                    // O "?" seria o else.
+        // O "?" seria o else.
         System.out.println(num);
 
 
         System.out.println("----- Aula 18 -----");
         // A18 - Switch
-            int num1 = 5;
-            switch (scanner_usuario){
-                case 1:
-                    System.out.println("O número é 1");
-                    break;
-                case 2:
-                    System.out.println("O número é 2");
-                    break;
-                case 5:
-                    System.out.println("O número é 5");
-                    break;
-                default:
-                    System.out.println("O número não é nem 1,2 ou 5, o número é " + scanner_usuario);
-                    break;
-                    // Serve para tratar situações em que o usuario digita algo que não está nos casos acima.
-                    // Sem default, ele não faz nada.
-            }
+        int num1 = 5;
+        switch (scanner_usuario) {
+            case 1:
+                System.out.println("O número é 1");
+                break;
+            case 2:
+                System.out.println("O número é 2");
+                break;
+            case 5:
+                System.out.println("O número é 5");
+                break;
+            default:
+                System.out.println("O número não é nem 1,2 ou 5, o número é " + scanner_usuario);
+                break;
+            // Serve para tratar situações em que o usuario digita algo que não está nos casos acima.
+            // Sem default, ele não faz nada.
+        }
     }
 }
