d79dcb4fb5dcfadbc9b72a98e1cf63ca0d7d6065
Guilherme Melo <112731659+GuiMelo012x@users.noreply.github.com>
2024-01-20 18:04:01 -0300

    Classe Cliente - Encapsulamento

---
+++ Orientação a Objetos/Modificadores de Acesso/Cliente.java
@@ -0,0 +1,13 @@
+package encapsulamento;
+
+public class Cliente {
+    String nome;
+    String endereço;
+
+    public Cliente(String nome, String endereço){
+        this.nome = nome;
+        this.endereço = endereço;
+    }
+
+
+}
