4c04dac23b87b12b45eeb442a8b9271ba52bf46e
Guilherme Melo <112731659+GuiMelo012x@users.noreply.github.com>
2024-01-20 17:54:06 -0300

    Classe Pessoa

---
+++ Orientação a Objetos/Introducao/Pessoa.java
@@ -0,0 +1,20 @@
+public class Pessoa {
+    String nome,cidade;
+    int ano_nascimento;
+    int idade;
+
+    public Pessoa(String nome, String cidade, int ano_nascimento, int idade){
+        this.nome = nome;
+        this.cidade = cidade;
+        this.ano_nascimento = ano_nascimento;
+        this.idade = idade;
+    }
+
+    public Pessoa(){
+
+    }
+    
+    void imprimir_informacoes (){
+        System.out.println(this.nome + " " + this.cidade + " " + this.ano_nascimento + " " + " " + this.idade + " - Método em Pessoa");
+    }
+}
