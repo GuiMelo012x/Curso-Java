2e235a3e62a27565c1554a69672eed3981c57c13
Guilherme Melo <112731659+GuiMelo012x@users.noreply.github.com>
2024-01-21 18:56:51 -0300

    Classe Ventilador - herda a Interface de IEletronico

---
+++ Orientação a Objetos/Interfaces/Ventilador.java
@@ -0,0 +1,20 @@
+package Interface;
+
+public class Ventilador implements IEletronico{
+    private boolean ligado = false;
+    @Override
+    public void ligar() {
+        if(!this.ligado){
+            this.ligado = true;
+            System.out.println("Ventilador ligado.");
+        }
+    }
+
+    @Override
+    public void desligar() {
+        if (this.ligado){
+            this.ligado = false;
+            System.out.println("Ventilador desligado.");
+        }
+    }
+}
