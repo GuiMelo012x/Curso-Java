2ff5b9602a0638562771ae178bc656c1ed5a9ad7
Guilherme Melo <112731659+GuiMelo012x@users.noreply.github.com>
2024-01-21 18:33:45 -0300

    Classe TreinamentoFinal que também herda os métodos abstratos de Treinamento

---
+++ Orientação a Objetos/Classes Abstratas/TreinamentoFinal.java
@@ -0,0 +1,17 @@
+package classes_abstratas;
+
+public class TreinamentoFinal extends Treinamento {
+    @Override
+    public void preparoFisico() {
+        System.out.println("Preparo físico de fim da temporada.");
+
+
+    }
+
+    @Override
+    public void jogoTreino() {
+        System.out.println("Jogo Treino de fim de temporada.");
+
+
+    }
+}
