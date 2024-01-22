6024acefcd0827add9d561e4e4e4bf503681565d
Guilherme Melo <112731659+GuiMelo012x@users.noreply.github.com>
2024-01-22 17:35:50 -0300

    Classe Caixa

---
+++ JavaLang/Object/Caixa.java
@@ -0,0 +1,20 @@
+package object_javalang;
+
+public class Caixa {
+    private Object[] objetos;
+    private int posicaoLivre;
+
+    public Caixa() {
+        objetos = new Object[100];
+        posicaoLivre = 0;
+    }
+
+    public void adicionar(Object nova){
+        this.objetos[this.posicaoLivre] = nova;
+        this.posicaoLivre++;
+    }
+
+    public Object pegar(int posicao){
+        return this.objetos[posicao];
+    }
+}
