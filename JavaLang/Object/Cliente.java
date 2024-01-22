3c68d831b8574f12b20a73eba5c72d95d6724eb5
Guilherme Melo <112731659+GuiMelo012x@users.noreply.github.com>
2024-01-22 17:36:07 -0300

    Classe Cliente

---
+++ JavaLang/Object/Cliente.java
@@ -0,0 +1,13 @@
+package object_javalang;
+
+public class Cliente extends Conta {
+    String nome;
+    String endereço;
+
+
+    public Cliente(String nome, String endereço){
+        this.nome = nome;
+        this.endereço = endereço;
+    }
+
+}
