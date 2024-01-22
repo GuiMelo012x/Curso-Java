f222564686a48c4f08840cd0ec1091ba3597184b
Guilherme Melo <112731659+GuiMelo012x@users.noreply.github.com>
2024-01-22 15:27:50 -0300

    Classe Cliente

---
+++ Executáveis e Documentação/Cliente.java
@@ -0,0 +1,13 @@
+package executaveis_jar;
+
+public class Cliente {
+    String nome;
+    String endereço;
+
+    public Cliente(String nome, String endereço){
+        this.nome = nome;
+        this.endereço = endereço;
+    }
+
+
+}
