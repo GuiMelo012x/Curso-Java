e330f96bade6e80f0cb90972f319b06d1c8da522
Guilherme Melo <112731659+GuiMelo012x@users.noreply.github.com>
2024-01-22 17:33:50 -0300

    usando a biblioteca Math do Java

---
+++ JavaLang/Biblioteca Math/BibliotecaMath.java
@@ -0,0 +1,16 @@
+package biblioteca_Math;
+
+public class BibliotecaMath {
+    public static void main(String[] args) {
+        System.out.println(Math.E);
+        System.out.println(Math.PI);
+        System.out.println(Math.sin(45));
+        System.out.println(Math.cos(45));
+        System.out.println(Math.tan(45));
+        System.out.println(Math.round(Math.PI));
+        System.out.println(Math.ceil(Math.PI));
+        System.out.println(Math.floor(Math.PI));
+        System.out.println(Math.pow(3,2));
+        System.out.println(Math.sqrt(25));
+    }
+}
