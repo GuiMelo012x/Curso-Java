2ea4f5c2fd2e2d4c7bbfc11116bc2d044fc52383
Guilherme Melo <112731659+GuiMelo012x@users.noreply.github.com>
2024-01-23 21:34:23 -0300

    Create BarraDeProgresso.java

---
+++ Threads/SemSincronia/BarraDeProgresso.java
@@ -0,0 +1,17 @@
+package semSincronia;
+
+public class BarraDeProgresso implements Runnable {
+    public void executa(){
+        // 10 milhões
+        for (int i = 0; i < 100; i++) {
+            System.out.println(i +  " - Barra de Progresso... aguarde");
+        }
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+            System.out.println( i + " - Barra de Progresso... aguarde");
+        }
+    }
+}
