ce8eca3384a23a3b2daaf885472e23618b14e43d
Guilherme Melo <112731659+GuiMelo012x@users.noreply.github.com>
2024-01-23 21:42:34 -0300

    Classe que possui o método que deposita.

---
+++ Threads/Threads Sincronizadas/FazDeposito.java
@@ -0,0 +1,13 @@
+package synchronizedThreads;
+
+public class FazDeposito implements Runnable {
+    private Conta conta;
+    public FazDeposito(Conta conta){
+        this.conta = conta;
+    }
+
+    @Override
+    public void run() {
+        this.conta.depositar(100f);
+    }
+}
