b75c8259316aa98602eddb4d3cde3008a6044b43
Guilherme Melo <112731659+GuiMelo012x@users.noreply.github.com>
2024-01-25 20:59:56 -0300

    Conjunto - Aula 107

---
+++ Estruturas de Dados/Conjuntos/Main.java
@@ -0,0 +1,21 @@
+package conjuntos;
+
+import java.util.LinkedList;
+
+public class Main { // Aula 107
+    public static void main(String[] args) {
+        Conjunto conjunto = new Conjunto();
+        conjunto.adiciona("America");
+        conjunto.adiciona("Fernanda");
+        conjunto.adiciona("Julia");
+        conjunto.adiciona("Fabricio");
+        conjunto.adiciona("Fabricio");
+
+
+        System.out.println(conjunto);
+
+
+
+
+    }
+}
