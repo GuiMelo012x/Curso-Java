b0534e727d1cfd77e07cc06376303ed54cc14089
Guilherme Melo <112731659+GuiMelo012x@users.noreply.github.com>
2024-02-01 13:29:42 -0300

    Create ClasseTeste.java

---
+++ Recursos Avançados/default_methods/ClasseTeste.java
@@ -0,0 +1,8 @@
+package default_methods;
+
+public class ClasseTeste implements Teste{
+    @Override
+    public String mensagem() {
+        return "Mensagem teste enviada pela ClasseTeste, método mensagem.";
+    }
+}
